fcc20ab38e543a38d22c9a69f680890a049cbe52
Liam <97658609+LiamBlog@users.noreply.github.com>
2025-11-27 08:48:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="background"></div>
     <Home />
     <footer>
-      <span>© 2025 Made in <a href="https://mlhh.cn" target="_blank">十安</a></span> | 
+      <span>© 2025 Made in <a href="https://mlhh.cn" target="_blank">十安 | </a></span>
       <a href="https://cloud.tencent.com/act/pro/eo-freeplan?ad_trace=3fe11a4b31634407819b427b61a2b768&from=28455&from_column=28455" target="_blank">腾讯云边缘安全加速EdgeOne</a>(国内)提供强劲加速 | 
       <a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2024077903号-3</a>
     </footer>
